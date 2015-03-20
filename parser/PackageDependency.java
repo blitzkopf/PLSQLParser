2a738a884eaa6eb63140f0f8be3d53beeaf9f04a
Yngvi Þór Sigurjónsson <blitzkopf@gmail.com>
2015-03-20 01:29:26 +0000

    Uppercasing identifiers when applicable

--- parser/PackageDependency.java
+++ parser/PackageDependency.java
@@ -16,20 +16,27 @@ class PackageDependency extends PLSQLBaseListener
 	String currentProc="";
 	ReferenceResolver resolver;
 
-	public PackageDependency(ReferenceResolver resolver) 
+	public PackageDependency(ReferenceResolver resolver,String schema) 
 	{
 		this.resolver=resolver;
+		this.currentSchema=schema;
+	}
+	public static String upUnq(String item) 
+	{
+		if(item.charAt(0) == '"') {
+			return item.substring(1,item.length()-2);
+		} else {
+			return item.toUpperCase();
+		}
 	}
 
 	public void enterCreate_package_body(PLSQLParser.Create_package_bodyContext ctx) 
 	{ 
-		System.out.println(ctx.getStart());
-		System.out.println(ctx.getStop());
+		//System.out.println(ctx.getStart());
+		//System.out.println(ctx.getStop());
 		if(ctx.schema_name != null) 
-			currentSchema=ctx.schema_name.getText();
-		else
-			currentSchema="UNKN";
-		currentPackage=ctx.package_name.getText();
+			currentSchema=upUnq(ctx.schema_name.getText());
+		currentPackage=upUnq(ctx.package_name.getText());
 		System.out.println("entering package "+currentSchema+"."+currentPackage);
 		resolver.registerElement(currentPackage,"package");
 		resolver.pushContext(currentPackage,"package");
@@ -46,8 +53,8 @@ class PackageDependency extends PLSQLBaseListener
 
 	public void enterVariable_declaration(PLSQLParser.Variable_declarationContext ctx) 
 	{ 
-		resolver.registerElement(ctx.variable_name.getText(),"variable");
-		System.out.println("variable:"+ctx.variable_name.getText());
+		resolver.registerElement(upUnq(ctx.variable_name.getText()),"variable");
+		//System.out.println("variable:"+ctx.variable_name.getText());
 		
 	}	
 
@@ -75,16 +82,16 @@ class PackageDependency extends PLSQLBaseListener
 	
 	public void enterProcedure_declaration_or_definition(PLSQLParser.Procedure_declaration_or_definitionContext ctx) 
 	{ 
-		System.out.println(ctx.getStart());
-		System.out.println(ctx.getStop());
-		handleEnterProcDef(ctx.procedure_name.getText());
+		//System.out.println(ctx.getStart());
+		//System.out.println(ctx.getStop());
+		handleEnterProcDef(upUnq(ctx.procedure_name.getText()));
 		
 	}
 	
 	
 	public void exitProcedure_declaration_or_definition(PLSQLParser.Procedure_declaration_or_definitionContext ctx) 
 	{ 
-		handleExitProcDef(ctx.procedure_name.getText());
+		handleExitProcDef(upUnq(ctx.procedure_name.getText()));
 		
 	}
 
@@ -94,88 +101,90 @@ class PackageDependency extends PLSQLBaseListener
 	}*/
 	public void enterFunction_declaration_or_definition(PLSQLParser.Function_declaration_or_definitionContext ctx) 
 	{ 
-		System.out.println(ctx.getStart());
-		System.out.println(ctx.getStop());
-		handleEnterProcDef(ctx.function_name.getText());	
+		//System.out.println(ctx.getStart());
+		//System.out.println(ctx.getStop());
+		handleEnterProcDef(upUnq(ctx.function_name.getText()));	
 	}
 
 	public void exitFunction_declaration_or_definition(PLSQLParser.Function_declaration_or_definitionContext ctx) 
 	{ 
-		handleExitProcDef(ctx.function_name.getText());
+		handleExitProcDef(upUnq(ctx.function_name.getText()));
 	}
 
 	public void handleProcFuncCall(List<PLSQLParser.CallContext> prefix,PLSQLParser.CallContext element) 
 	{ 
 		String calledSchema =  "";
 		String calledPackage = "";
-		String calledProc = element.id.getText();
+		String calledProc = upUnq(element.id.getText());
 		PLSQLElement ref;
 		if(prefix.size()==2) {
-			calledSchema=prefix.get(0).id.getText();
-			calledPackage=prefix.get(1).id.getText();
+			calledSchema=upUnq(prefix.get(0).id.getText());
+			calledPackage=upUnq(prefix.get(1).id.getText());
 		} else if (prefix.size()==1) {
 			calledSchema=currentSchema;
-			calledPackage=prefix.get(0).id.getText();;
+			calledPackage=upUnq(prefix.get(0).id.getText());
 		}
-		ref=resolver.findElement(element.id.getText());
+		ref=resolver.findElement(upUnq(element.id.getText()));
+		/*
 		if(ref != null ) 
 			System.out.println(ref.name+" <"+ref.type+">");
-
-		System.out.println(calledPackage);
-		System.out.println(currentSchema+"."+currentPackage+"."+currentProc+"->"+calledSchema+"."+calledPackage+"."+calledProc );
+		*/
+		//System.out.println(calledPackage);
+		System.out.println("\""+currentSchema+"."+currentPackage+"."+currentProc+"\"->\""+calledSchema+"."+calledPackage+"."+calledProc +"\"");
 	}
 
 	public void enterVariable_or_function_call(PLSQLParser.Variable_or_function_callContext ctx) 
 	{ 
 		//handleCallContext(ctx);
 		handleProcFuncCall(ctx.prefix,ctx.element);
-		System.out.println("Prefix: " + ctx.prefix);
-		System.out.println("Function_call: " + ctx.element.getText());
+		//System.out.println("Prefix: " + ctx.prefix);
+		//System.out.println("Function_call: " + ctx.element.getText());
 	}
 	
 	public void enterCall_statement(PLSQLParser.Call_statementContext ctx) 
 	{ 
 		//handleCallContext(ctx.getRuleContext(PLSQLParser.LvalueContext.class,0));
 		handleProcFuncCall(ctx.prefix,ctx.element);
-		System.out.println("Prefix: " + ctx.prefix);
-		System.out.println("Procedure call: " + ctx.element.getText());
+		//System.out.println("Prefix: " + ctx.prefix);
+		//System.out.println("Procedure call: " + ctx.element.getText());
 	}
 
-	public static void parse(CharStream stream,ReferenceResolver resolver) {
-    try {
-        PLSQLLexer lex = new PLSQLLexer(stream);
-        CommonTokenStream tokens = new CommonTokenStream(lex);
-       /* System.out.println(tokens);
+	public static void parse(CharStream stream,ReferenceResolver resolver, String schema) 
+	{
+    	try {
+	        PLSQLLexer lex = new PLSQLLexer(stream);
+	        CommonTokenStream tokens = new CommonTokenStream(lex);
+	       /* System.out.println(tokens);
 
-        for( int i = 0; i < tokens.size();i++) {
-            Token tk = tokens.get(i);
-            System.out.println(tk);  
-        }*/
+	        for( int i = 0; i < tokens.size();i++) {
+	            Token tk = tokens.get(i);
+	            System.out.println(tk);  
+	        }*/
 
-        PLSQLParser parser = new PLSQLParser(tokens);
+	        PLSQLParser parser = new PLSQLParser(tokens);
 
-        PLSQLParser.FileContext fileContext = parser.file();
-        ParseTreeWalker walker = new ParseTreeWalker();
-		
-    	PackageDependency listener = new PackageDependency(resolver);
+	        PLSQLParser.FileContext fileContext = parser.file();
+	        ParseTreeWalker walker = new ParseTreeWalker();
+			
+	    	PackageDependency listener = new PackageDependency(resolver,schema);
 
-    	walker.walk(listener, fileContext);       
+	    	walker.walk(listener, fileContext);       
 
-    } catch (RecognitionException e) {
-        System.err.println(e.toString());
-    } catch (java.lang.OutOfMemoryError e) {
-        //System.err.println(file + ":");
-        System.err.println(e.toString());
-    } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-        //System.err.println(file + ":");
-        System.err.println(e.toString());
-    }       
-}
+	    } catch (RecognitionException e) {
+	        System.err.println(e.toString());
+	    } catch (java.lang.OutOfMemoryError e) {
+	        //System.err.println(file + ":");
+	        System.err.println(e.toString());
+	    } catch (java.lang.ArrayIndexOutOfBoundsException e) {
+	        //System.err.println(file + ":");
+	        System.err.println(e.toString());
+	    }       
+	}
 
 	public static void main(String args[])
 	throws IOException
 	{
- 		parse(new ANTLRFileStream(args[0]),new ReferenceResolver());
+ 		parse(new ANTLRFileStream(args[0]),new ReferenceResolver(),"");
 
 	}
 
--- parser/ReferenceResolver.java
+++ parser/ReferenceResolver.java
@@ -17,22 +17,22 @@ public class ReferenceResolver
 	Hashtable<String,PLSQLElement> itemMap=new Hashtable<String,PLSQLElement>();
 	public void registerElement(String name,String type)
 	{
-		System.out.println("-> putting " + name);
+		//System.out.println("-> putting " + name);
 		itemMap.put(name,new PLSQLElement(name,type));
 	}
 
 	public PLSQLElement findElement(String name) {
-		System.out.println("<- getting " + name);
+		//System.out.println("<- getting " + name);
 		return itemMap.get(name);
 	}
 
 	public void pushContext(String name,String type) {
-		System.out.println("push");
+		//System.out.println("push");
 
 	}
 
 	public void popContext() {
-		System.out.println("pop");
+		//System.out.println("pop");
 	}
 
 };
